c1f3a504ff287c02de55205f687d69a6befecf83
wdtch <rever.psif@live.jp>
2015-06-19 17:53:40 +0900

    test for variable, function, function-prototype declaration and definition

--- parsertest.py
+++ parsertest.py
@@ -96,73 +96,29 @@ int main (int a) {
 testcode6 = """
 int i;
 int j;
+int *k;
+int array[8];
+int *p_array[5];
 
+int func1(int a, int b);
+void func2();
+int func1(int i); /* duplicate but type consistent prototype */
+void func1(); /* duplicate and type inconsistent prototype */
+int func3(int a, int *array);
+void func4(int a, int a); /* duplicate param declaration */
 
 int main() {
     int i;
     return i;
 }
+
+/* duplicate function definition */
+int main(int i) {
+    return i;
+}
+
+/* duplicate with variable declaration */
+void i() {
+    ;
+}
 """
-
-# result1 = """
-# int main() {
-#     int i;
-#     data[0] = 3;
-#     data[1] = 5;
-#     data[2] = 10;
-#     data[3] = 7;
-#     data[4] = 1;
-#     data[5] = 4;
-#     data[6] = 12;
-#     data[7] = 6;
-#     bubble_sort(data);
-#     for (i = 0; i < AMOUNT; i++) {
-#         print(&data[i]);
-
-#     }
-#     return 0;
-
-# }
-# """
-
-# result2 = """
-# void func(int a, int b) {
-#     int i;
-#     i = 0;
-#     while (i < a) {
-#         i += b;
-
-#     }
-
-# }
-
-# """
-
-# result3 = """
-# void bubble_sort(int *data) {
-#     int i, j, tmp;
-#     for (i = 0; i < AMOUNT - 1; i++) {
-#         for (j = AMOUNT - 1; j > i; j--) {
-#     if (data[j - 1] > data[j]) {
-#         tmp = data[j];
-#     data[j] = data[j - 1];
-#     data[j - 1] = tmp;
-
-#     }
-
-#     }
-
-#     }
-
-# }
-# """
-
-# class Test(unittest.TestCase):
-#     def setUp(self):
-#         pass
-
-#     def tearDown(self):
-#         pass
-
-#     def test_parse_and_restore(self):
-#         self.assertEqual(parser.Parser().build(), result1)
