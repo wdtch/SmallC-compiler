ae0a34180583d1c666e0d03547e6226367196f4b
wdtch <rever.psif@live.jp>
2015-07-17 19:20:26 +0900

    test for printcode.py

---
+++ printcode_test.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import nose
+from unittest import TestCase
+import codegen
+import printcode
+
+
+class PrintCodeTest(TestCase):
+
+    def setUp(self):
+        self.printer = printcode.PrintCode([])
+
+    def tearDown(self):
+        pass
+
+    def test_instr(self):
+        """Instruction型のインスタンスを文字列に変換するテスト"""
+        expected = "    li $t0,0\n"
+
+        instr = codegen.Instruction("li", ("$t0", 0))
+        actual = self.printer.struct_to_string(instr)
+
+        nose.tools.eq_(expected, actual)
+
+    def test_label(self):
+        """Label型のインスタンスを文字列に変換するテスト"""
+        expected = "main:\n"
+
+        label = codegen.Label("main")
+        actual = self.printer.struct_to_string(label)
+
+        nose.tools.eq_(expected, actual)
+
+    def test_code_to_string1(self):
+        """アセンブリのリストを文字列に変換するテスト(Instructionのみ)"""
+        expected = "    li $t0,0\n" + \
+                   "    li $t1,1\n"
+
+        code = [codegen.Instruction("li", ("$t0", 0)),
+                codegen.Instruction("li", ("$t1", 1))]
+        self.printer.code = code
+        actual = self.printer.code_to_string()
+
+        nose.tools.eq_(expected, actual)
+
+    def test_code_to_string2(self):
+        """アセンブリのリストを文字列に変換するテスト(Instructions, Label)"""
+        expected = "main:\n" + "    li $t0,0\n"
+
+        code = [codegen.Label("main"),
+                codegen.Instruction("li", ("$t0", 0))]
+        self.printer.code = code
+        actual = self.printer.code_to_string()
+
+        nose.tools.eq_(expected, actual)
+
+if __name__ == '__main__':
+    nose.main(argv=['nose', '-v'])
