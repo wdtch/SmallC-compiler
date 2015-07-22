28e09c51f001ad086a50131999e6897f34747b4d
wdtch <rever.psif@live.jp>
2015-07-22 22:25:11 +0900

    Before change of intermed code

--- assign_address.py
+++ assign_address.py
@@ -36,43 +36,98 @@ class AssignAddress(object):
                     else:
                         self.intermed_code[i].params[j].var.offset = self.ofs_to_param()
 
-                self.ofsman.reset()
+                # self.ofsman.reset()
 
                 # 関数内での宣言
-                for k, decl in enumerate(itmdelem.body.decls):
-                    if isinstance(decl.var.objtype, tuple) and \
-                        decl.var.objtype[0] == "array":
-                        self.intermed_code[i].body.decls[k].var.offset = self.ofs_to_arrayvar()
-                    else:
-                        self.intermed_code[i].body.decls[k].var.offset = self.ofs_to_var()
+                # for k, decl in enumerate(itmdelem.body.decls):
+                #     if isinstance(decl.var.objtype, tuple) and \
+                #         decl.var.objtype[0] == "array":
+                #         self.intermed_code[i].body.decls[k].var.offset = self.ofs_to_arrayvar()
+                #     else:
+                #         self.intermed_code[i].body.decls[k].var.offset = self.ofs_to_var()
 
                 # self.ofsman.reset()
 
                 # 関数内の複文に含まれる宣言
                 # for stmt in itmdelem.body.stmts:
-                #     self.assign_address_to_stmt(stmt)
-                # self.assign_ofs_to_compstmt(itmdelem.body)
+                    # self.assign_address_to_stmt(stmt)
+                self.assign_address_to_compstmt(itmdelem.body)
 
-                self.ofsman.reset()
+                # self.ofsman.reset()
 
         return self.intermed_code
+
+    def assign_address_to_compstmt(self, compstmt):
+        """引数として取った複文の宣言と、文に含まれる一時変数の宣言にアドレスを割り当てる"""
+        # 宣言
+        for decl in compstmt.decls:
+            if isinstance(decl.var.objtype, tuple) and \
+                decl.var.objtype[0] == "array":
+                if decl.var.offset == -1:
+                    decl.var.offset = self.ofs_to_arrayvar(decl.var.objtype[2])
+            else:
+                if decl.var.offset == -1:
+                    decl.var.offset = self.ofs_to_var()
+
+        # self.ofsman.reset()
+
+        # 複文(を含む可能性のある中間表現)に含まれる複文に再帰的にアドレス割り当て
+        for stmt in compstmt.stmts:
+            if isinstance(stmt, ic.CompoundStatement):
+                self.assign_address_to_compstmt(stmt)
+
+            elif isinstance(stmt, ic.IfStatement):
+                if isinstance(stmt.then_stmt, ic.CompoundStatement):
+                    self.assign_address_to_compstmt(stmt.then_stmt)
+                else:
+                    # とりあえずprintだけ
+                    print(stmt.then_stmt)
+                    print(stmt.then_stmt.__dict__)
+
+                if isinstance(stmt.else_stmt, ic.CompoundStatement):
+                    self.assign_address_to_compstmt(stmt.else_stmt)
+                else:
+                    # とりあえずprintだけ
+                    print(stmt.else_stmt)
+                    print(stmt.else_stmt.__dict__)
+
+            elif isinstance(stmt, ic.WhileStatement):
+                if isinstance(stmt, ic.CompoundStatement):
+                    self.assign_address_to_compstmt(stmt.stmt)
+                else:
+                    print(stmt.stmt)
+                    print(stmt.stmt.__dict__)
+
+            elif isinstance(stmt, ic.LetStatement):
+                if stmt.var.kind == "temp" and stmt.var.offset == -1:
+                    stmt.var.offset = self.ofs_to_var()
+                if isinstance(stmt.exp, ic.VarExpression) and stmt.exp.var.offset == -1:
+                    stmt.exp.var.offset = self.ofs_to_var()
+
+            elif isinstance(stmt, ic.CallStatement) and stmt.dest.offset == -1:
+                stmt.dest.offset = self.ofs_to_var()
+
+        return compstmt
 
     def ofs_to_var(self):
         """局所変数に割り当てるアドレスを計算し、その値を返す"""
         self.ofsman.next_ofs_var()
         offset_var = self.ofsman.current_varoffset
+
         return offset_var
 
     def ofs_to_param(self):
         """パラメータに割り当てるアドレスを計算し、その値を返す"""
         self.ofsman.next_ofs_param()
         offset_param = self.ofsman.current_paramoffset
+
         return offset_param
 
     def ofs_to_globalvar(self):
         """グローバル変数に割り当てるアドレスを計算し、その値を返す"""
         self.ofsman.next_ofs_var()
         offset_globalvar = self.ofsman.current_varoffset
+
         return offset_globalvar
 
     def ofs_to_arrayvar(self, size):
